9836b4105df3610fd49e1c58d8b287be1622d83e
zacariasaderito <aderitozacarias123@hotmail.com>
2019-08-26 01:04:40 +0200

    Practice for exam 2

--- src/test/java/Payroll/repository/demographic/GenderRepositoryTest.java
+++ src/test/java/Payroll/repository/demographic/GenderRepositoryTest.java
@@ -1,32 +1,76 @@
 package Payroll.repository.demographic;
 
+import Payroll.domain.demographic.Gender;
+import Payroll.factory.demographic.GenderFactory;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.HashMap;
 
 import static org.junit.Assert.*;
 
 public class GenderRepositoryTest {
 
+    private GenderRepository repository;
+    private Gender gender;
+    private String id;
+
+
+    @Before
+    public void setUp() throws Exception {
+        repository = GenderRepository.getRepository();
+        gender = GenderFactory.getInstance("Male");
+        id = gender.getId();
+    }
+
     @Test
     public void getRepository() {
+        assertNotNull(repository);
     }
 
     @Test
     public void create() {
+
+        Gender result = repository.create(gender);
+        id = result.getId();
+
+        assertNotNull(result);
+        System.out.println("Created");
     }
 
     @Test
     public void update() {
+
+        Gender result = repository.create(gender);
+        Gender toReplace = repository.update(gender);
+        assertNotNull(toReplace);
+        System.out.println("Updated");
     }
 
     @Test
     public void read() {
+        Gender result = repository.create(gender);
+        Gender toRead = repository.read(id);
+        assertNotNull(toRead);
+        System.out.println("Read");
     }
 
     @Test
     public void delete() {
+        Gender result = repository.create(gender);
+        repository.delete(id);
+        assertNull(repository.read(id));
+        System.out.println("Deleted");
     }
 
     @Test
     public void getAll() {
+
+        Gender result = repository.create(gender);
+        Gender result1 = repository.create(GenderFactory.getInstance("Female"));
+        Gender result2 = repository.create(GenderFactory.getInstance("Male"));
+
+        assertTrue(repository.getAll().contains(result));
+
     }
 }
--- src/test/java/Payroll/service/demographic/GenderServiceTest.java
+++ src/test/java/Payroll/service/demographic/GenderServiceTest.java
@@ -1,32 +1,69 @@
 package Payroll.service.demographic;
 
+import Payroll.domain.demographic.Gender;
+import Payroll.factory.demographic.GenderFactory;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class GenderServiceTest {
 
+    private GenderService service;
+    private Gender gender;
+
+    @Before
+    public void setUp() throws Exception {
+        service = GenderService.getService();
+        gender = GenderFactory.getInstance("Famele");
+    }
     @Test
     public void getService() {
+        assertNotNull(service);
+        System.out.println("Factory working");
     }
 
     @Test
     public void create() {
+        Gender result = service.create(gender);
+        assertSame(gender, result);
+        System.out.println("Created");
+
     }
 
     @Test
     public void update() {
+        Gender result = service.create(gender);
+        Gender toReplace = service.update(gender);
+        assertEquals(result.getId(), toReplace.getId());
+
+        System.out.println("Updated");
     }
 
     @Test
     public void read() {
+        Gender result = service.create(gender);
+        service.read(result.getId());
+        assertNotNull(result);
+        System.out.println("");
     }
 
     @Test
     public void delete() {
+        Gender result = service.create(gender);
+        service.delete(result.getId());
+        assertNull(service.read(result.getId()));
+        System.out.println("Deleted");
     }
 
     @Test
     public void getAll() {
+        Gender result1 = service.create(GenderFactory.getInstance("Famele"));
+        Gender result2 = service.create(GenderFactory.getInstance("Male"));
+        Gender result3 = service.create(GenderFactory.getInstance("Famele"));
+
+        assertTrue(service.getAll().contains(result1));
+        System.out.println("Read All");
+
     }
 }
